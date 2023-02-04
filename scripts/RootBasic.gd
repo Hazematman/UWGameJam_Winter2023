1f6753c572faf0e9b18fc3f80fef9cb607b21e2e
Lucas Fryzek <lucas.fryzek@gmail.com>
2023-02-04 16:17:09 -0500

    Make roots clickable again

--- scripts/RootBasic.gd
+++ scripts/RootBasic.gd
@@ -7,6 +7,13 @@ extends Node2D
 
 var impulse = Vector2(200,0)
 
+signal clicked
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Bot.apply_impulse(Vector2(0,0), impulse)
+
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.pressed:
+		emit_signal("clicked")
